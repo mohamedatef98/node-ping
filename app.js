753c221377a714efab8682d400dbfcf1f35a5710
Mohamed Atef <eng.mohamedatef98@gmail.com>
2021-01-02 22:20:03 +0200

    Fix index.html path

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
+var path = require('path')
 
 
 app.get('/', function (req, res) {
-    var file = fs.readFileSync('./index.html', 'UTF-8')
+    var file = fs.readFileSync(path.join(__dirname, 'index.html'), 'UTF-8')
     res.status(200)
     res.setHeader('Content-Type', 'text/html')
     res.send(file)
