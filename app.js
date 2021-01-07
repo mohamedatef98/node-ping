5f6f7a28225bc00b4a37333f5b783ed94774de4e
Mohamed Atef <eng.mohamedatef98@gmail.com>
2021-01-07 12:59:32 +0200

    Fix Static serving in express

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ var app = express()
 var fs = require('fs')
 var path = require('path')
 
-const staticDir = path.join(__dirname, 'static')
-
-app.get('/', function (req, res) {
-    var file = fs.readFileSync(
-        path.join(staticDir, 'index.html'), 
-        'UTF-8'
-    )
-    res.status(200)
-    res.setHeader('Content-Type', 'text/html')
-    res.send(file)
-    res.end()
-})
+app.use(express.static('static'))
 
 app.listen(80, function() { console.log('Server is running') })
