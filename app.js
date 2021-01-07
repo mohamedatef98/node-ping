908c023a4994790311da6febe66642be43676d6e
Mohamed Atef <eng.mohamedatef98@gmail.com>
2021-01-07 12:54:45 +0200

    Add "useful" content

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var app = express()
 var fs = require('fs')
 var path = require('path')
 
+const staticDir = path.join(__dirname, 'static')
 
 app.get('/', function (req, res) {
-    var file = fs.readFileSync(path.join(__dirname, 'index.html'), 'UTF-8')
+    var file = fs.readFileSync(
+        path.join(staticDir, 'index.html'), 
+        'UTF-8'
+    )
     res.status(200)
     res.setHeader('Content-Type', 'text/html')
     res.send(file)
